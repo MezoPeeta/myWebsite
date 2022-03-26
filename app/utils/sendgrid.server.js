e8741164597e448379072dd87fa65efcc23e2a3b
MezoPeeta <wildlifemain1@gmail.com>
2022-03-26 20:17:54 +0200

    k

--- app/utils/sendgrid.server.js
+++ app/utils/sendgrid.server.js
@@ -1,4 +1,4 @@
 import sgMail from '@sendgrid/mail';
 
 export default sgMail;
-sgMail.setApiKey('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');
+sgMail.setApiKey(process.env.TOKEN);
